05604d479a6654aeb7b4597e8cb74a1412127b5b
Derek Tamsen <dtamsen@gmail.com>
2020-04-16 23:37:34 -0700

    loop through list of packages and deps to install

--- main.go
+++ main.go
@@ -2,7 +2,10 @@
 // It only prints what it would do using a static list.
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 // Pkg defines the package name, version, and dependencies
 type Pkg struct {
@@ -10,6 +13,9 @@ type Pkg struct {
 	version      string
 	dependencies []Pkg
 }
+
+// installList is a list of full package names
+type pkgList []string
 
 // MakeFakeList returns a list of fake packages and deps
 func MakeFakeList() *Pkg {
@@ -29,8 +35,37 @@ func MakeFakeList() *Pkg {
 	return &pkgs
 }
 
+// fullName returns the name + version of the package
+func (p *Pkg) fullName() string {
+	return fmt.Sprintf("%v_%v", p.name, p.version)
+}
+
+// install fake installs packages from installList
+func install(l pkgList) error {
+	for _, pkg := range l {
+		log.Printf("Installing %s", pkg)
+	}
+	return nil
+}
+
+// getList returns a list of packages given Pkg
+func getList(p *Pkg) pkgList {
+	installPkgs := pkgList{p.fullName()}
+
+	for _, pkg := range p.dependencies {
+		installPkgs = append(installPkgs, pkg.fullName())
+	}
+
+	return installPkgs
+}
+
 func main() {
 	pkgs := MakeFakeList()
 
-	log.Println(pkgs)
+	installPkgs := getList(pkgs)
+
+	err := install(installPkgs)
+	if err != nil {
+		log.Panicln(err)
+	}
 }
