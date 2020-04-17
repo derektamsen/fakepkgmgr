331994e8eb6c8d10b58bb9e7a7c910b4ef51a64d
Derek Tamsen <dtamsen@gmail.com>
2020-04-16 22:07:02 -0700

    refactor fake list into make function

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ package main
 
 import "log"
 
+// pkg defines the package name, version, and dependencies
 type pkg struct {
 	name         string
 	version      string
 	dependencies []pkg
 }
 
-func main() {
+func makeFakeList() *pkg {
 	pkgs := pkg{
 		name:    "golang",
 		version: "2:1.12~1ubuntu1",
@@ -23,6 +24,12 @@ func main() {
 				dependencies: []pkg{{name: "gcc", version: "4:9.2.1-3.1ubuntu1"}}},
 		},
 	}
+
+	return &pkgs
+}
+
+func main() {
+	pkgs := makeFakeList()
 
 	log.Println(pkgs)
 }
